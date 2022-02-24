b76aba129a75dc25e5a7e90426af86b21c6773ec
Alcazaba <99512955+Alcazaba@users.noreply.github.com>
2022-02-24 23:19:45 +0100

    Sections labels + handicap

--- js/carousel.js
+++ js/carousel.js
@@ -1,14 +1,30 @@
 /*initialization*/
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, {
-        duration : 1000,
-        dist : 0,
-        /*shift : ,*/
-        /*padding : ,*/
-        numVisible : 4,
-        fullWidth : true,
+document.addEventListener("DOMContentLoaded", function () {
+	var elems = document.querySelectorAll(".carousel");
+	var instances = M.Carousel.init(elems, {
+		duration: 1000,
+		dist: 0,
+		padding: 100,
+		numVisible: 5,
+		fullWidth: false,
+		indicators: true,
+	});
 
-    });
-  });
+	/*autoplay*/
+	let indicatorItems = document.querySelectorAll(".carousel .indicator-item"),
+		slideTime = 3000,
+		activeClass = "active";
 
+	setInterval(() => {
+		indicatorItems.forEach((el) => {
+			if (el.classList.contains(activeClass)) {
+				sib = el.nextElementSibling;
+				if (sib == null) {
+					indicatorItems[0].click();
+				} else {
+					sib.click();
+				}
+			}
+		});
+	}, slideTime);
+});
