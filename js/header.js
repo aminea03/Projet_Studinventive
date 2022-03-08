a16a3cc4e15d7d92b0df78450ba43269ec92685d
Alcazaba <99512955+Alcazaba@users.noreply.github.com>
2022-03-08 12:31:26 +0100

    MOBILE nav OK

--- js/header.js
+++ js/header.js
@@ -1,12 +1,24 @@
-/* nav "Formation" dropdown initializer */
-document.addEventListener("DOMContentLoaded", function () {
-	var elems = document.querySelectorAll(".dropdown-trigger");
-	var instances = M.Dropdown.init(elems, {
-		hover: true,
-		coverTrigger: false,
-		autoTrigger: false,
-		onCloseEnd: function () {
-			document.activeElement.blur();
-		},
+/* Desktop nav "Formation" dropdown initializer */
+if (window.matchMedia("(min-width: 1024px)").matches) {
+	document.addEventListener("DOMContentLoaded", function () {
+		var elems = document.querySelectorAll(".dropdown-trigger");
+		var instances = M.Dropdown.init(elems, {
+			hover: true,
+			coverTrigger: false,
+			autoTrigger: false,
+			onCloseEnd: function () {
+				document.activeElement.blur();
+			},
+		});
 	});
-});
+}
+
+/* Mobile sidenav initializer */
+if (window.matchMedia("(max-width: 1023px)").matches) {
+	document.addEventListener("DOMContentLoaded", function () {
+		var elems = document.querySelectorAll(".sidenav");
+		var instances = M.Sidenav.init(elems, {
+			edge: "right",
+		});
+	});
+}
