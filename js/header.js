1b6110caaedb482ffc32c98a04305a2667d0dbe7
Alcazaba <99512955+Alcazaba@users.noreply.github.com>
2022-04-07 20:12:48 +0200

    FAQ Desktop

--- js/header.js
+++ js/header.js
@@ -1,7 +1,9 @@
 /* Desktop nav "Formation" dropdown initializer */
 
 document.addEventListener("DOMContentLoaded", function () {
-	var elems = document.querySelectorAll(".dropdown-trigger");
+	var elems = document.querySelectorAll(
+		".dropdown-trigger:not(.nohover div .dropdown-trigger)"
+	);
 	var instances = M.Dropdown.init(elems, {
 		hover: true,
 		coverTrigger: false,
